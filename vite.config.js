4fa26a262dd7fb2fe9e82919de07db572a706008
saharshtripathi64-lab <saharshtripathi64@gmail.com>
2026-09-23 14:14:31 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base : "vite-project"
+  plugins: [react(), tailwindcss()]
 })
+
+
+
